b3a1dc339ffd6e2e533b34a7bace670ea8121e80
danilolucasmd <danilolucasmd@gmail.com>
2021-08-04 16:19:42 -0300

    Move pixel font to the fonts folder

--- src/utils.ts
+++ src/utils.ts
@@ -14,7 +14,7 @@ export function getRandomInt(max: number) {
 }
 
 export function lerp (value1: number, value2: number, amount: number) {
-	amount = amount < 0 ? 0 : amount;
-	amount = amount > 1 ? 1 : amount;
-	return value1 + (value2 - value1) * amount;
+  amount = amount < 0 ? 0 : amount;
+  amount = amount > 1 ? 1 : amount;
+  return value1 + (value2 - value1) * amount;
 };
